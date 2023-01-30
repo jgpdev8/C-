7ddb546fa6e67da90804ee597ee79bbf81ba7e0a
jgpdev8 <123153789+jgpdev8@users.noreply.github.com>
2023-01-30 16:54:35 +0100

    NotaValida
    Filtra si la nota es aprobada, suspensa o no válida

---
+++ NotaValida/NotaValida/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace NotaValida
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double nota;
+            Console.WriteLine("Dime la nota que has sacado en Programación");
+            nota = double.Parse(Console.ReadLine());
+            if(nota>=0 && nota <= 10)
+            {
+                if(nota < 5)
+                {
+                    Console.WriteLine("Suspenso");
+                }
+                else
+                {
+                    Console.WriteLine("Aprobado");
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Nota no válida");
+            }
+        }
+
+    }
+}
