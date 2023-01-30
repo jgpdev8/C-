0a0520d191003641a0697429b891273f9dc2dac1
jgpdev8 <123153789+jgpdev8@users.noreply.github.com>
2023-01-30 16:56:40 +0100

    Add files via upload

---
+++ CentigradoFarenhairt/CentigradoFarenhairt/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CentigradoFarenhairt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string opcion;
+            double grados;
+            Console.WriteLine("¿Que grados quieres convertir, Centigrados o Farenheit");
+            opcion = Console.ReadLine();
+            Console.WriteLine("Dime los grados");
+            grados = double.Parse(Console.ReadLine());
+
+            if (opcion == "centigrados")
+            {
+                grados = (grados * 9 / 5) + 32;
+                Console.WriteLine(grados);
+            }
+            else
+            {
+                grados = (grados - 32) * 5/9;
+                Console.WriteLine(grados);
+            }
+        }
+    }
+}
