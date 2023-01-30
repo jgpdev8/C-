8c24290285c41990fd6c7712d783f8b9fcd1501a
jgpdev8 <123153789+jgpdev8@users.noreply.github.com>
2023-01-30 16:56:15 +0100

    Add files via upload

---
+++ PositivoOnegativo/PositivoOnegativo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace PositivoOnegativo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.WriteLine("dime un número");
+            num = int.Parse(Console.ReadLine());
+            if(num == 0)
+            {
+                Console.WriteLine("El numero es cero");
+            }else if (num < 0)
+            {
+                Console.WriteLine("El numero es negativo");
+            }
+            else Console.WriteLine("el numero es positivo");
+        }
+    }
+}
