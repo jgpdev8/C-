f8b84833d1fe35f46dccb9f0b62e208dc742fadc
jgpdev8 <123153789+jgpdev8@users.noreply.github.com>
2023-01-30 16:55:35 +0100

    Add files via upload

---
+++ ParOimpar/ParOimpar/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ParOimpar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.WriteLine("Dime un número");
+            num = int.Parse(Console.ReadLine());
+            if(num % 2 == 0)
+            {
+                Console.WriteLine("el número es par");
+            }
+            else Console.WriteLine("el número es impar");
+        }
+    }
+}
